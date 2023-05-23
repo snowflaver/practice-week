82ab1f587dc45ecdd3536c7505ebe08db570df17
snowflaver <sirayukiyui@gmail.com>
2023-05-23 10:37:25 +0900

    CRA
    yarn 설치

---
+++ week-1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* 여기가 메인 화면 */}
+      <p>hello react</p>
+    </div>
+  );
+}
+
+export default App;
